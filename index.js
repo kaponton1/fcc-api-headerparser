350d8ec824da403de2f56954c966e8771bd51ae8
Kenny Ponton <kendelafunk@gmail.com>
2024-09-01 13:24:00 +0000

    feat: Added whoami endpoint for req information details

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let ip = req.ip
+  let lang = req.headers['accept-language']
+  let software = req.headers['user-agent']
+
+  res.json({ ipaddress: ip, language: lang, software: software})
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
